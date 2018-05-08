a3134e7a45cd802afcd71ef4513d6ed974812ed4
GhostlyLeo <leothemusicghost@outlook.com>
2018-05-08 15:24:54 +0200

    It works now, feel free to use it on Linux, Changed from a .sh shortcut to a .desktop shortcut because of issues with sudo and stuff

--- Main.py
+++ Main.py
@@ -21,38 +21,53 @@ url = input(": ")
 if not url.startswith("http"):
     url = "http://" +  url
 
-name = input("Please name the package (When adding spaces, please add \\'s).")
-fullPath = "/usr/share/" + name + "/"
-shortcutName = name + ".sh"
-desktopPath = "~/Desktop/" + shortcutName
+name = input("Please name the package (When adding spaces, please add \\'s).") # Name for the app
+fullPath = "/usr/share/" + name + "/" # The full path to the dir of the app
+shortcutName = name + ".desktop" # The name for the shortcut
+desktopPath = "~"
 
 if not name.endswith(".py"):
     fileName = name + ".py"
 
-print("Downloading file...\n0% ||||||||||", end="")
-fullFile = requests.get(url)
-file_ = fullFile.text
-print("Downloaded file.\n25% ///|||||||", end="")
+# THIS IS THE SHORTCUT CONTENT
+shortcutContent = """[Desktop Entry]
+Name=""" + name + """
+Comment=Installed via Python Package Manager
+Exec=python3 /usr/share/""" + name + "/" + fileName + """
+Terminal=true
+Type=Application
+"""
 
-if not os.path.exists(fullPath) and not os.path.exists(desktopPath):
-    os.chdir("/usr/share/")
-    print("Making Directory\n25% ///|||||||", end="")
-    os.makedirs(name)
-    print("Made Directory\n50% /////|||||", end="")
-    os.chdir(name)
-    print("Dropping file...\n50% /////|||||")
-    with open(fileName, "w") as pyFile:
-        pyFile.write(file_)
-    print("Dropped file.\n75% ////////||")
+os.system("clear") # All os.system("clear")'s means that I clear the terminal so it looks nicer.
+print("\nDownloading file...\n0% ||||||||||", end="")
+fullFile = requests.get(url) # Get the info from the URL.
+file_ = fullFile.text # Get the contents from the URL
+os.system("clear") # Clear
+print("\nDownloaded file.\n25% ///|||||||", end="")
 
-    os.chdir("~/Desktop/")
-    print("Writing shortcut...\n75% ////////||")
-    with open(fileName, "w") as shFile:
-        shFile.write("python3 " + fullPath + """
-        exit""")
-    print("Complete!")
-    
+if not os.path.exists(fullPath): 
+    os.chdir("/usr/share/") # Change directory to /usr/share
+    os.system("clear")
+    print("\nMaking Directory\n25% ///|||||||", end="")
+    os.makedirs(name) # Make a directory called the input you inputted before
+    os.system("clear")
+    print("\nMade Directory\n50% /////|||||", end="")
+    os.chdir(name) # Change directory to that file
+    os.system("clear")
+    print("\nDropping file...\n50% /////|||||", end="")
+    with open(fileName, "w") as pyFile: # Create a new .py file
+        pyFile.write(file_) # Write the info to that new .py file
+    os.system("clear") 
+    print("\nDropped file.\n75% ////////||", end="")
 
+    os.chdir(os.path.expanduser("~")) # Change directory to the home directory
+    os.chdir("Desktop") # Change directory to the desktop directory
+    os.system("clear")
+    print("\nWriting shortcut...\n75% ////////||", end="")
+    with open(shortcutName, "w") as shFile: # Write the shortcut using the content from above.
+        shFile.write(shortcutContent)
+    os.system("clear")
+    print("\rComplete! 100% //////////") # Done!
 else:
     print("The directory already exists.")
 
